46ae4847d5e33303dfe3e4e308aafc7188b437e3
andykr1k <69021102+andykr1k@users.noreply.github.com>
2025-07-02 21:05:33 -0700

    Changed button titles

--- src/App.jsx
+++ src/App.jsx
@@ -270,13 +270,13 @@ function App() {
             onClick={handleDownload}
             className="flex-1 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
           >
-            Download HTML
+            Generate Signature
           </button>
           <button
             onClick={copyToClipboard}
             className="flex-1 px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700"
           >
-            Copy HTML
+            Source Code
           </button>
         </div>
       </div>
